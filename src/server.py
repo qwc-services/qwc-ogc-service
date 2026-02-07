2bce6f96619d17fa91215b5bd605e6f998b63991
Sandro Mani <manisandro@gmail.com>
2026-02-07 12:56:20 +0100

    Allow setting local webserver via FLASK_RUN_PORT

--- src/server.py
+++ src/server.py
@@ -234,4 +234,4 @@ if __name__ == '__main__':
     print("Starting OGC service...")
     from flask_cors import CORS
     CORS(app)
-    app.run(host='localhost', port=5013, debug=True)
+    app.run(host='localhost', port=os.environ.get("FLASK_RUN_PORT", 5000), debug=True)
